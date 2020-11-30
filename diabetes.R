698fe4e52b0de0b64b2bbd36bfc55ef3a8bddeb6
anorris772 <43426457+anorris772@users.noreply.github.com>
2020-11-30 14:17:18 -0600

    New scatter plots
    Plots for comparing rates across race and gender.

--- diabetes.R
+++ diabetes.R
@@ -11,35 +11,62 @@
 # Read in the national diabetes data set from the CDC, covers 1980 to 2017
 archive = read.csv("C:/Users/Alex/Desktop/Fall20/Stat6000/data/National_Data.csv", header=TRUE)
 
-
 # Throughout this project the Tidyverse package for R is utilized
 # install.packages("tidyverse")
 library(tidyverse)
 
-
 # Start by narrowing down to just new diagnoses
 # Fetch all rows that are classified as "newly diagnosed"
 # 'indicatorid' of 2 represents a group that is newly diagnosed with diabetes
-# Also filter by 'estimateid' of 2 to get rate per 1000 people
+# Also filter by 'estimateid' of 72 to get rate per 1000 people
 newly_diagnosed = dplyr::filter(archive, indicatorid == 2 & estimateid == 72)
 
+# select just the year, race, and estimate attributes from the data
 year_race_rate_estimate = dplyr::select(newly_diagnosed, yearid, raceid, Estimate)
-
-
-
-
-
-plot(year_race_rate_estimate)
-ggplot(data = year_race_rate_estimate, aes(x = yearid, y = Estimate))
-
-
-
 
 # Select just the races from the newly_diagnosed table)
 race = dplyr::select(newly_dignosed, raceid)
-# find unique entires in the race table
+# find unique entries in the race table
 unique(race)
-# only race 0(total), 1(white), 2(black), and 7(Hispanic) 
-white = dplyr::filter(year_race_rate_estimate, raceid == 1)
-black = dplyr::filter(year_race_rate_estimate, raceid == 2)
-hisp = dplyr::filter(year_race_rate_estimate, raceid == 7)
+# unique races: 0(total), 1(white), 2(black), and 7(Hispanic) 
+
+
+# First visualize diabetes rates across the 3 included races
+# Tidyverse + ggplot scatter plot 
+# Filter out total race and total age, but keep total gender
+
+tidy_archive <- archive %>%
+  filter(indicatorid == 2 & 
+           estimateid == 72 &
+           raceid != 0 & 
+           genderid == 0 & 
+           ageid == 99 & Estimate != ".")
+
+# Plot newly diagnosed diabetes estimates across races
+ggplot(tidy_archive, aes(x = yearid, 
+                         y = as.numeric(Estimate), 
+                         col = as.factor(raceid))) + 
+        geom_point(size = 3) +
+        xlab("Year") + ylab("Diabetes Estimate Per 1000") +
+        labs(title = "Rates of Diabetes Across Races Over Time", col="Race") + 
+        scale_color_hue(labels = c("White", "Black", "Hispanic"))
+
+
+# Next visualize the diabetes rates across gender
+# Filter out total age and total gender, but keep total race
+tidy_archive <- archive %>%
+  filter(indicatorid == 2 & 
+           estimateid == 72 &
+           raceid == 0 & 
+           genderid != 0 & 
+           ageid == 99 & Estimate != ".")
+
+# Plot newly diagnosed diabetes estimates across gender
+ggplot(tidy_archive, aes(x = yearid, 
+                         y = as.numeric(Estimate), 
+                         col = as.factor(genderid))) + 
+      geom_point(size = 3) +
+      xlab("Year") + ylab("Diabetes Estimate Per 1000") +
+      labs(title = "Rates of Diabetes Across Gender Over Time", col="Gender") + 
+      scale_color_hue(labels = c("Male", "Female"))
+  
